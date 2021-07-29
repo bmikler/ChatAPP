2d012cb591dec3cf68a60e932b361c140aaaa6d6
Bartlomiej Mikler <bartlomiej.mikler@gmail.com>
2021-07-29 11:01:21 +0200

    autorefreshing chatbox

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
-
+import os
 from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 from cs50 import SQL
 
 
 # Configure application
+
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 Session(app)
 
 # Connect do SQL - TODO
@@ -16,12 +19,10 @@ db = SQL("sqlite:///messeges.db")
 
 @app.route("/")
 def index():
-    messeges = db.execute("SELECT * FROM messeges")
-
     if not session.get("name"):
         return redirect("/login")
 
-    return render_template("index.html", messeges=messeges)
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -37,10 +38,15 @@ def logout():
     session["name"] = None
     return redirect("/")
 
-
 @app.route("/send", methods=["GET", "POST"])
 def send():
     text = request.form.get("message")
     if text:
         db.execute("INSERT INTO messeges (sender, text) VALUES(?, ?)", session["name"], text)
     return redirect("/")
+
+    
+@app.route("/chatbox")
+def chatbox():
+    messeges = db.execute("SELECT * FROM messeges")
+    return render_template("chatbox.html", messeges=messeges)
